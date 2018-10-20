622cd8a29638d2ad8a89682ce076cf581ef07f1f
Elias Alioui <supreme.e99@icloud.com>
2018-10-20 21:44:44 +0200

    Player and Console

--- Plugin Sample/src/de/mywork/plugin/Commands.java
+++ Plugin Sample/src/de/mywork/plugin/Commands.java
@@ -3,6 +3,7 @@ package de.mywork.plugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 
 public class Commands implements CommandExecutor {
@@ -12,7 +13,34 @@ public class Commands implements CommandExecutor {
 
 		if(command.getName().equalsIgnoreCase("opfer")) {
 			
+			if(sender instanceof Player) {
+				
+				Player player = (Player) sender;
+				
+				
+				if(args.length == 0) {
+					
+					player.sendMessage("do you want to know if ur ein opfer? dann schreib nochmal opfer");
+					
+					
+				}
+					if(args.length == 1 && args[0].equalsIgnoreCase("opfer")) {
+						
+						player.sendMessage("ur an opfer");
+						
+					} else
+						if(args.length == 2 && args[1].equalsIgnoreCase("?")) {
+							
+							player.sendMessage("u even questioning? \nshame on u");
+							
+						}
+				
+			} else {
+			
+			
 			if(args.length == 0) {
+				
+	
 				
 				System.out.println("do you want to know if ur ein opfer?");
 				
@@ -27,6 +55,7 @@ public class Commands implements CommandExecutor {
 						System.out.println("u even questioning? \nshame on u");
 						
 					}
+			}
 					
 			
 		}
